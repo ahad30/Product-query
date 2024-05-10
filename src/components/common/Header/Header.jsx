5910c3d4c0cfd1e6af871c5013820f38a84c5ad7
ahad30 <mohiminulislamahad@gmail.com>
2024-05-10 19:18:22 +0600

    firebase added

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -176,7 +176,7 @@ const Header = () => {
                 />
                  <Tooltip id="my-tooltip" />
               </div>
-            </div>
+            </div>   
             <ul
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white text-black rounded-box w-52'
